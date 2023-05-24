26372012899cabe5ca1492aa3b370d65570600be
Gargant <gargant0373@gmail.com>
2023-05-24 12:00:23 +0200

    Improved user usability.
    Added a fallback command and help command.

--- src/main/java/me/gargant/commands/GTimeCommand.java
+++ src/main/java/me/gargant/commands/GTimeCommand.java
@@ -1,5 +1,9 @@
 package me.gargant.commands;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import masecla.mlib.annotations.RegisterableInfo;
@@ -9,6 +13,7 @@ import masecla.mlib.main.MLib;
 import me.gargant.classes.RunEnd;
 import me.gargant.containers.MapViewContainer;
 import me.gargant.services.RunService;
+import net.md_5.bungee.api.ChatColor;
 
 @RegisterableInfo(command = "gtime")
 public class GTimeCommand extends Registerable {
@@ -18,6 +23,29 @@ public class GTimeCommand extends Registerable {
     public GTimeCommand(MLib lib, RunService runService) {
         super(lib);
         this.runService = runService;
+    }
+
+    @Override
+    public void fallbackCommand(CommandSender sender, String[] args) {
+        String message = "&fRunning &c&lG&e&lTime &fv" + lib.getPlugin().getDescription().getVersion();
+        message = ChatColor.translateAlternateColorCodes('&', message);
+        String help = "&f/gtime &chelp?";
+        help = ChatColor.translateAlternateColorCodes('&', help);
+
+        sender.sendMessage(message);
+        sender.sendMessage(help);
+    }
+
+    @SubcommandInfo(subcommand = "help", permission = "gtime.help")
+    public void handleHelp(Player player) {
+        List<String> help = new ArrayList<>();
+        help.add("&c&lG&e&lTime &fv" + lib.getPlugin().getDescription().getVersion());
+        help.add("&c/gtime &estart <map> &7- Start a run");
+        help.add("&c/gtime &eend &7- End a run");
+        help.add("&c/gtime &etimes &7- View your times");
+        help.add("&c/gtime &ehelp &7- View this help page");
+
+        help.forEach(c -> player.sendMessage(ChatColor.translateAlternateColorCodes('&', c)));
     }
 
     @SubcommandInfo(subcommand = "end", permission = "gtime.use")
